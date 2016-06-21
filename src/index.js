d742942a7729594681bb4c97ab6a7033a753ec07
Arkadiusz Buras <macbury@gmail.com>
2016-06-21 09:46:05 +0200

    Ignore generated js

--- src/index.js
+++
@@ -1,5 +0,0 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
-var App_1 = require('./components/App');
-ReactDOM.render(React.createElement(App_1.App, null), document.getElementById('root'));
-//# sourceMappingURL=index.js.map
